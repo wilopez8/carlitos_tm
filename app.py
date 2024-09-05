391a3b511827d9ed964682cd777b9f9dc7d6a2d0
wilmar lopez <wilopez8@yahoo.com>
2024-09-04 20:38:44 -0400

    este es el commit de los archivos de carlitos

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import cv2
+import numpy as np
+#from PIL import Image
+from PIL import Image as Image, ImageOps as ImagOps
+from keras.models import load_model
+
+model = load_model('keras_model.h5')
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+st.title("Reconocimiento de Imágenes")
+image = Image.open('OIG5.jpg')
+st.image(image, width=350)
+with st.sidebar:
+    st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
+img_file_buffer = st.camera_input("Toma una Foto")
+
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+   #To read image file buffer as a PIL Image:
+    img = Image.open(img_file_buffer)
+
+    newsize = (224, 224)
+    img = img.resize(newsize)
+    # To convert PIL Image to numpy array:
+    img_array = np.array(img)
+
+    # Normalize the image
+    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    print(prediction)
+    if prediction[0][0]>0.5:
+      st.header('Izquierda, con Probabilidad: '+str( prediction[0][0]) )
+    if prediction[0][1]>0.5:
+      st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
+    #if prediction[0][2]>0.5:
+    # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
+
+
